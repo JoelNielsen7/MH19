7e5d07425e2d885b8f64a0f81b0fab3d5309c104
azhulogic <43792633+azhulogic@users.noreply.github.com>
2019-02-02 23:59:06 -0600

    add calc_dT

--- score.py
+++ score.py
@@ -22,3 +22,31 @@ def scre(farm, sensor_number, ):
     pSTD = stdev(pressurelist)
     windMAG   = abs(2.7- data.wid_speed)
     cloudMAG = abs(.5-data.clouds)
+
+import math
+
+def calc_dT(temp, humidity, pressure):
+    #celsius, percentage, millibar
+
+    dewPoint = ((humidity / 100) ** (1/8)) * (112 + 0.9 * temp) + 0.1 * temp - 112
+    vapour = 6.112 * (math.e ** ((17.67 * dewPoint) / (243.5 + dewPoint)))
+
+    tHigher = temp
+    tLower = dewPoint
+    wetGuess = dewPoint
+
+    while True:
+        previousGuess = wetGuess
+        wetGuess = (tLower + tHigher) / 2
+        if abs(previousGuess - wetGuess) < 0.005:
+            break
+        EwGuess = 6.112 * (math.e ** ((17.67 * wetGuess) / (243.5 + wetGuess)))
+        ActVpGuess = EwGuess - (0.00066 * (1 + 0.00115 * wetGuess) * (temp - wetGuess) * pressure)
+        EDelta = vapour - ActVpGuess
+        if EDelta < 0:
+            tHigher = wetGuess
+        else:
+            tLower = wetGuess
+
+    dT = temp - wetGuess
+    return dT
