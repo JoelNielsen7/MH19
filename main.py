57f11b479410082f4b9ae7efa5d8534b764c60d7
Dominic Voto <votox002@umn.edu>
2019-02-02 23:56:24 -0600

    Add main function to main.py

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from datapoint import DataPoint
 from sensor import Sensor
 from farm import Farm
 
-dp1 = DataPoint(19, 2, 90, 1.5, .4, 6,5)
-dp2 = DataPoint(25, 10, 257, 2.4, .1, 9,5)
+def main():
+    dp1 = DataPoint(19, 2, 90, 1.5, .4, 6,5)
+    dp2 = DataPoint(25, 10, 257, 2.4, .1, 9,5)
 
-s1 = Sensor((20.0,50.0))
-s1.update(dp1)
-s1.update(dp2)
+    s1 = Sensor((20.0,50.0))
+    s1.update(dp1)
+    s1.update(dp2)
 
-s1.add_water((47.0,88.0))
-print(s1.water_angles)
+    s1.add_water((47.0,88.0))
+    print(s1.water_angles)
 
-f1 = Farm((0.0,90.0))
-f1.add_sensor(s1)
+    f1 = Farm((0.0,90.0))
+    f1.add_sensor(s1)
+
+if __name__ == "__main__":
+    main()
