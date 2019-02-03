6d3281acbd20d314e5923676537b17cac825309a
JebBush <sciguy16@gmail.com>
2019-02-03 10:45:18 -0600

    Added score

--- score.py
+++ score.py
@@ -9,118 +9,29 @@ from sensor import calculate_angle
 
 """ need to find out how to get the correct data set"""
 ### takes in a farm object the sensor it wants to use and then the day
-def score(farm, data, sensor):
-    pressurelist=[]
-    for sens in farm.sensors:
-        pressurelist.append(sens.point.pressure)
+def score(scorelist):
+    avg = svg(scorelist)
 
-    deltaT = calc_dT(data.temp, data.humidity, data.pressure)
-    DT_VAL = norm(deltaT, 15)
-
-    tempMAG = abs(16.666-data.temp)
-    TEMP_VAL = norm(tempMAG, 20)
-
-    pSTD_VAL = 0
-    if len(pressurelist) > 1:
-        pSTD = statistics.stdev(pressurelist)
-        pSTD_VAL = norm(pSTD, 50)
-
-    windMAG   = abs(2.7- data.wind_speed)
-    wing_VAL = norm(windMAG, 10)
-
-    cloudMAG = abs(.5-data.clouds)
-    cloud_VAL = norm(cloudMAG, .5)
-
-    rainAM = data.rain
-    rain_VAL = norm(rainAM, 150)
-
-    deltaA_VAL = 0
-
-    deltaA = 0
-    if sensor.water_angle != None:
-        angle = sensor.water_angle
-        deltaA = angle - data.wind_dir
-        if deltaA < 0:
-            deltaA = abs(deltaA)
-        if deltaA > 180:
-            deltaA = 360 - deltaA
-
-    deltaA= norm(deltaA, 180)
-    deltaA_VAL = 1 - deltaA
-    deltaA_VAL = norm(deltaA, 180)
-
-    score = cloud_VAL+wing_VAL+DT_VAL+pSTD_VAL+TEMP_VAL-deltaA_VAL
-
-    return(score)
-
-def responsetext(data, sensor,bool, farm):
-    scorelist = []
-    pressurelist=[]
-    for sens in farm.sensors:
-        pressurelist.append(sens.point.pressure)
-
-    deltaT = calc_dT(data.temp, data.humidity, data.pressure)
-    DT_VAL = norm(deltaT, 15)
-    scorelist.append(DT_VAL)
-
-    tempMAG = abs(16.666-data.temp)
-    TEMP_VAL = norm(tempMAG, 20)
-    scorelist.append(TEMP_VAL)
-
-    pSTD_VAL = 0
-    if len(pressurelist) > 1:
-        pSTD = statistics.stdev(pressurelist)
-        pSTD_VAL = norm(pSTD, 50)
-    scorelist.append(pSTD_VAL)
-
-    windMAG   = abs(2.7- data.wind_speed)
-    wing_VAL = norm(windMAG, 10)
-    scorelist.append(wing_VAL)
-
-    cloudMAG = abs(.5-data.clouds)
-    cloud_VAL = norm(cloudMAG, .5)
-    scorelist.append(cloud_VAL)
-
-    rainAM = data.rain
-    rain_VAL = norm(rainAM, 150)
-    scorelist.append(rain_VAL)
-
-    deltaA = 0
-    if sensor.water_angle != None:
-        angle = sensor.water_angle
-        deltaA = angle - data.wind_dir
-        if deltaA < 0:
-            deltaA = abs(deltaA)
-        if deltaA > 180:
-            deltaA = 360 - deltaA
-
-    deltaA= norm(deltaA, 180)
-    deltaA_VAL = 1 - deltaA
-    scorelist.append(deltaA_VAL)
-
-    score = cloud_VAL+wing_VAL+DT_VAL+pSTD_VAL+TEMP_VAL+deltaA_VAL+rain_VAL
-
-    avg = score/6
-
-    arraycount = 0
-
-    narray = ["evaporation rate is nice ","temp is nice","pressure is nice","wind is looking good","nice cloud mix","not to rainy","wind is not pointing towards water"]
-    parray = ["asdf","asdf","asdf","asdf","asdf","asdf",]
-
+    parray = ["\n The temperature is took to keep particles together","\n The wind will help move the pesticide but not blow it away","\n The evaporation rate will increase the effectivenss of your spray ","\n There are few drafts and gusts","\n there is no rain forecasted", "\n The pesticide will not blow into any water"]
+    narray = ["\n the temprature is to high","\n It is very windy", "\n"+"The evaporation rate is high","\n The pressure diffrence is high","\n it is a bit raint","\n The wind is pointing towards a close body of water",]
+    temp wind dt pressure rain da
     if bool:
-        responsetext = " here "
+        responsetext = "We recommend spraying because"
+        neg = "\n Maybe hold off becuase"
 
-        for val in scorelist:
-            if val<avg:
-                responsetext+= narray[arraycount]
-            arraycount+=1
-    else:
-        responsetext = " We should spray today becuase "
+        sortlist = scorelist
+        sortlist.sort()
+        top = sortlist[len(sortlist)-1]
+        second = sortlist[len(sortlist)-2]
 
-        for val in scorelist:
-            if val> avg:
-                responsetext+= narray[arraycount]
-        arraycount+=1
+        bad = sortlist[0]
+
+        responsetext+= parray[scorelist.index(top)]
+        responsetext+= parray[scorelist.index(second)]
+        responsetext+=neg
+        responsetext+= narray[scorelist.index(bad)]
+
+
 
 
 
@@ -153,6 +64,7 @@ def calc_dT(temp, humidity, pressure):
             tLower = wetGuess
 
     dT = temp - wetGuess
+    print(dT)
     return dT
 
 
@@ -176,7 +88,7 @@ def ranker(farm, sensor):
     for datapoint in farm.future_forecast:
         scorelistOG.append(score(farm, datapoint, sensor))
 
-    #print(scorelistOG)
+
 
     timecounter = .00
     scorelist = []
@@ -187,8 +99,8 @@ def ranker(farm, sensor):
         timecounter+=.05/8
     return(scorelist)
 
-
+# returns index of the max
 def maxfinder(scorelist):
-    max = max(scorelist)
-    distance = scorelist.index(max)
+    maxbit = max(scorelist)
+    distance = scorelist.index(maxbit)
     return(distance)
